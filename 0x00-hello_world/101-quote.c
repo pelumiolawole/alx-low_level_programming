186cfaf7d953cbc23ac6c88c3138281ea6f1cb2e
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-03-10 23:07:32 +0100

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,4 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
-    int res = chdir (cwd);
-/* I promise I don't need to check this return value.  */
-(void) res;
 }

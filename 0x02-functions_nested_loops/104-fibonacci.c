9c8bf73e63a582a503c24ef6696eb17b9d2b4710
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-03-15 12:45:53 +0100

    Signed-off-by: Pelumi Olawole <olawolepelumisunday@gmail.com>

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	long int i, x = 1, y = 2, sum = 0;
 
-	for (i = 0; i < 96; i++)
+	for (i = 0; i < 94; i++)
 	{
 		printf("%lu, ", x);
 		sum = x + y;
 		x = y;
 		y = sum;
-		if (i == 95)
+		if (i == 93)
 			printf("%lu\n", x);
 	}
 	return (0);

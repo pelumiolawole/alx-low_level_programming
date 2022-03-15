eaa0d1fde82aa25caf2023feb9d94457077e147a
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-03-16 00:53:14 +0100

    Signed-off-by: Pelumi Olawole <olawolepelumisunday@gmail.com>

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 #include <stdio.h>
 void positive_or_negative(int i);
-int largest_number(in a, int b, int c);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-
-
-#endif /* main.h */
-
+#endif

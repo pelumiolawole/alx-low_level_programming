b77c14ea1cbb8e872ecec2e635000f63b2305ef5
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-04-21 23:42:51 +0100

    Signed-off-by: Pelumi Olawole <olawolepelumisunday@gmail.com>

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,24 @@
 #include "lists.h"
-
+#include <stdio.h>
 
 /**
- * print_list - print counter all elements of `list_t` list
- * @h: head of linked list
- * Return: counter number of nodes
+ * print_list - print elements of linkedlist
+ * @h: linked list
+ *
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t nbr_nodes = 0;
+	const list_t *current = h;
 
-	while (h != NULL)
+	while (current != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		i++;
-		h = h->next;
+		printf("[%d] %s\n", current->len,
+		current->str != NULL ? current->str : "(nil)");
+		current = current->next;
+		nbr_nodes++;
 	}
-
-	return (i);
+	return (nbr_nodes);
 }

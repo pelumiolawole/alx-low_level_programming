164d52ee8d89783acaad7cfa189c60279f3c8ece
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-06-14 01:34:19 +0100

    Giga millions won

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.compelumiolawole/alx-low_level_programming/main/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD="$PWD/../libinjection.so"
+gcc *.o -shared -o libruin.so
+LD_PRELOAD=$PWD/libruin.so
